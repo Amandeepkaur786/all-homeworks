d27b794d68d51ede353b9391002d89fa4551eb60
aman <aman99625@gmail.com>
2019-07-21 14:29:29 -0400

    helloworld program

---
+++ coding/helloworldprog1.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : helloworldprog1.cpp
+// Author      : AMAN
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "helloworld" << endl; // prints bubblesort
+	return 0;
+}
